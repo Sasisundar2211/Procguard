e397c3a6521dbe3909fc9aee926e0bf563f85077
dj taylor <djtaylor@djs-MacBook-Air.local>
2026-01-10 12:17:02 +0530

    Refactor Vercel deployment to use api/index.py entrypoint and zero-config rewrites

--- api/index.py
+++ api/index.py
@@ -1,8 +1,1 @@
-import sys
-import os
-
-# Add the project root to sys.path so we can import 'app'
-# This is critical for Vercel Serverless Functions to find the 'app' folder
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
 from app.main import app
